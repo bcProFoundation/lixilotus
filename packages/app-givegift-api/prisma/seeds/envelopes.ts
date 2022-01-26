044bccbc3de70ff1ed17aec84bcb31605d1f3932
vince8x <vince.tran8x@gmail.com>
2022-01-26 17:07:08 +0700

    Change seed to add image

--- packages/app-givegift-api/prisma/seeds/envelopes.ts
+++ packages/app-givegift-api/prisma/seeds/envelopes.ts
@@ -22,8 +22,8 @@ export const envelopes = [
   },
   {
     name: "New Year 4",
-    thumbnail: "images/2d/2df78ebd3044c3711f988493dfd8b43c814fdbfaada6ad9c715550892f8188d7-200.png",
-    image: "images/2d/2df78ebd3044c3711f988493dfd8b43c814fdbfaada6ad9c715550892f8188d7.png",
+    thumbnail: "images/2d/e5ad03d3c3405d98376477e1e70ee82b31a3190b900b84e6ca1555b846a8fd51-200.png",
+    image: "images/2d/e5ad03d3c3405d98376477e1e70ee82b31a3190b900b84e6ca1555b846a8fd51.png",
     slug: "new-year-4",
     description: "Happy New Year",
   }
