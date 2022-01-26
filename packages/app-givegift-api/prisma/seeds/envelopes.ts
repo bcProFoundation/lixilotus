2a954837de06b0389f2ca2477609295890de0a03
viet <viet@tribeaccelerator.co>
2022-01-26 16:50:15 +0700

    Update seed images

--- packages/app-givegift-api/prisma/seeds/envelopes.ts
+++ packages/app-givegift-api/prisma/seeds/envelopes.ts
@@ -14,24 +14,17 @@ export const envelopes = [
     description: "Happy New Year",
   },
   {
-    name: "New Year 2",
-    thumbnail: "images/2d/2df78ebd3044c3711f988493dfd8b43c814fdbfaada6ad9c715550892f8188d7-200.png",
-    image: "images/2d/2df78ebd3044c3711f988493dfd8b43c814fdbfaada6ad9c715550892f8188d7.png",
-    slug: "new-year-2",
+    name: "New Year 3",
+    thumbnail: "images/74/74dbb5869f8a84dbd59379d99c85085fe441faf8adf5f06c61664ecaccfbb002-200.png",
+    image: "images/74/74dbb5869f8a84dbd59379d99c85085fe441faf8adf5f06c61664ecaccfbb002.png",
+    slug: "new-year-3",
     description: "Happy New Year",
   },
   {
-    name: "New Year 2",
+    name: "New Year 4",
     thumbnail: "images/2d/2df78ebd3044c3711f988493dfd8b43c814fdbfaada6ad9c715550892f8188d7-200.png",
     image: "images/2d/2df78ebd3044c3711f988493dfd8b43c814fdbfaada6ad9c715550892f8188d7.png",
-    slug: "new-year-2",
-    description: "Happy New Year",
-  },
-  {
-    name: "New Year 2",
-    thumbnail: "images/2d/2df78ebd3044c3711f988493dfd8b43c814fdbfaada6ad9c715550892f8188d7-200.png",
-    image: "images/2d/2df78ebd3044c3711f988493dfd8b43c814fdbfaada6ad9c715550892f8188d7.png",
-    slug: "new-year-2",
+    slug: "new-year-4",
     description: "Happy New Year",
   }
 ];
