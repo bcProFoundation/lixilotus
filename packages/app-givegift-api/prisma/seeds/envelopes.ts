f0b8a33dc5cfca3a6f22da2273a5d8b67d1b8c23
doantrung1197 <doantrung1197@gmail.com>
2022-02-14 08:24:31 +0700

    - Update new gif envelope

--- packages/app-givegift-api/prisma/seeds/envelopes.ts
+++ packages/app-givegift-api/prisma/seeds/envelopes.ts
@@ -26,5 +26,26 @@ export const envelopes = [
     image: "images/e5/e5ad03d3c3405d98376477e1e70ee82b31a3190b900b84e6ca1555b846a8fd51.png",
     slug: "new-year-4",
     description: "Happy New Year",
-  }
+  },
+  {
+    name: "Brown Rice",
+    thumbnail: "images/4b/4bf059e8479191ae6d3763cdf50f9c6b9d835db86be1b2af0168fcd62cb2100c-200.png",
+    image: "images/4b/4bf059e8479191ae6d3763cdf50f9c6b9d835db86be1b2af0168fcd62cb2100c.png",
+    slug: "brown-rice",
+    description: "Brown Rice",
+  },
+  {
+    name: "Chocolate",
+    thumbnail: "images/73/73de403c4e847e30f221aa206409c34c7b2467d3f77aa7723366fd978ee513fc-200.png",
+    image: "images/73/73de403c4e847e30f221aa206409c34c7b2467d3f77aa7723366fd978ee513fc.png",
+    slug: "chocolate",
+    description: "Chocolate",
+  },
+  {
+    name: "Heart",
+    thumbnail: "images/5a/5aab278ea3971ad367c57ee0d6216d8dcc7d512869bd27fa66c19a9c380f775d-200.png",
+    image: "images/5a/5aab278ea3971ad367c57ee0d6216d8dcc7d512869bd27fa66c19a9c380f775d.png",
+    slug: "heart",
+    description: "Heart",
+  },
 ];
