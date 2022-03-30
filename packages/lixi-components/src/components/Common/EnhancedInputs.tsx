c4f3f6b051dfc1447cafb0a735c8f60f0aad6c51
Trung Doan <doantrung1197@gmail.com>
2022-03-30 03:06:25 +0000

    - Update Scan QR Claim code and Upload QR Code

--- packages/lixi-components/src/components/Common/EnhancedInputs.tsx
+++ packages/lixi-components/src/components/Common/EnhancedInputs.tsx
@@ -9,76 +9,77 @@ import ScanQRCode from './ScanQRCode';
 // import useBCH from '@hooks/useBCH';
 import { currency } from './Ticker';
 import { LockOutlined } from '@ant-design/icons';
+import UploadQRCode from './UploadQRCode';
 
 export const AntdFormCss = css`
-    .ant-input-group-addon {
-        background-color: ${props =>
-    props.theme.forms.addonBackground} !important;
-        border: 1px solid ${props => props.theme.forms.border};
-        color: ${props => props.theme.forms.addonForeground} !important;
-    }
-    input.ant-input,
-    textarea.ant-input,
-    .ant-select-selection,
-    .ant-select-item-option-active
-    .ant-select-item {
-        background-color: ${props =>
-    props.theme.forms.selectionBackground} !important;
-        box-shadow: none !important;
-        border-radius: 4px;
-        font-weight: bold;
-        color: ${props => props.theme.forms.text};
-        opacity: 1;
-        height: 50px;
-    }
-    .ant-input-affix-wrapper {
-        background-color: ${props => props.theme.forms.selectionBackground};
-        border: 1px solid ${props => props.theme.wallet.borders.color} !important;
-    }
+  .ant-input-group-addon {
+    background-color: ${props => props.theme.forms.addonBackground} !important;
+    border: 1px solid ${props => props.theme.forms.border};
+    color: ${props => props.theme.forms.addonForeground} !important;
+    width: 99px;
+  }
+  input.ant-input,
+  textarea.ant-input,
+  .ant-select-selection,
+  .ant-select-item-option-active
+  .ant-select-item {
+      background-color: ${props =>
+  props.theme.forms.selectionBackground} !important;
+    box-shadow: none !important;
+    border-radius: 4px;
+    font-weight: bold;
+    color: ${props => props.theme.forms.text};
+    opacity: 1;
+    height: 50px;
+  }
+  .ant-input-affix-wrapper {
+    background-color: ${props => props.theme.forms.selectionBackground};
+    border: 1px solid ${props => props.theme.wallet.borders.color} !important;
+  }
+  .ant-select-selector {
+    height: 60px !important;
+    border: 1px solid ${props => props.theme.wallet.borders.color} !important;
+    align-items: center;
+  }
+  .ant-form-item-has-error
+    > div
+    > div.ant-form-item-control-input
+    > div
+    > span
+    > span
+    > span.ant-input-affix-wrapper {
+    background-color: ${props => props.theme.forms.selectionBackground};
+    border-color: ${props => props.theme.forms.error} !important;
+  }
+
+  .ant-form-item-has-error .ant-input,
+  .ant-form-item-has-error .ant-input-affix-wrapper,
+  .ant-form-item-has-error .ant-input:hover,
+  .ant-form-item-has-error .ant-input-affix-wrapper:hover {
+    background-color: ${props => props.theme.forms.selectionBackground};
+    border-color: ${props => props.theme.forms.error} !important;
+  }
+
+  .ant-form-item-has-error
+    .ant-select:not(.ant-select-disabled):not(.ant-select-customize-input)
     .ant-select-selector {
-        height: 60px !important;
-        border: 1px solid ${props => props.theme.wallet.borders.color} !important;
-        align-items: center;
-    }
-    .ant-form-item-has-error
-        > div
-        > div.ant-form-item-control-input
-        > div
-        > span
-        > span
-        > span.ant-input-affix-wrapper {
-        background-color: ${props => props.theme.forms.selectionBackground};
-        border-color: ${props => props.theme.forms.error} !important;
-    }
-
-    .ant-form-item-has-error .ant-input,
-    .ant-form-item-has-error .ant-input-affix-wrapper,
-    .ant-form-item-has-error .ant-input:hover,
-    .ant-form-item-has-error .ant-input-affix-wrapper:hover {
-        background-color: ${props => props.theme.forms.selectionBackground};
-        border-color: ${props => props.theme.forms.error} !important;
-    }
-
-    .ant-form-item-has-error
-        .ant-select:not(.ant-select-disabled):not(.ant-select-customize-input)
-        .ant-select-selector {
-        background-color: ${props => props.theme.forms.selectionBackground};
-        border-color: ${props => props.theme.forms.error} !important;
-    }
-    .ant-select-single .ant-select-selector .ant-select-selection-item,
-    .ant-select-single .ant-select-selector .ant-select-selection-placeholder {
-        line-height: 60px;
-        text-align: left;
-        color: ${props => props.theme.forms.text};
-        font-weight: bold;
-    }
-    .ant-form-item-has-error .ant-input-group-addon {
-        color: ${props => props.theme.forms.error} !important;
-        border-color: ${props => props.theme.forms.error} !important;
-    }
-    .ant-form-item-explain.ant-form-item-explain-error {
-        color: ${props => props.theme.forms.error} !important;
-    }
+    background-color: ${props => props.theme.forms.selectionBackground};
+    border-color: ${props => props.theme.forms.error} !important;
+  }
+  .ant-select-single .ant-select-selector .ant-select-selection-item,
+  .ant-select-single .ant-select-selector .ant-select-selection-placeholder {
+    line-height: 60px;
+    text-align: left;
+    color: ${props => props.theme.forms.text};
+    font-weight: bold;
+  }
+  .ant-form-item-has-error .ant-input-group-addon {
+    color: ${props => props.theme.forms.error} !important;
+    border-color: ${props => props.theme.forms.error} !important;
+  }
+  .ant-form-item-explain.ant-form-item-explain-error {
+    color: ${props => props.theme.forms.error} !important;
+  }
 `;
 
 export const AntdFormWrapper = styled.div`
@@ -89,37 +90,50 @@ type InputAddonTextProps = {
 } & React.HTMLProps<HTMLSpanElement> & React.HTMLAttributes<HTMLElement>
 
 export const InputAddonText = styled.span<InputAddonTextProps>`
-    width: 100%;
-    height: 100%;
-    display: block;
+  width: 100%;
+  height: 100%;
+  display: block;
 
-    ${props =>
-    props.disabled
-      ? `
-      cursor: not-allowed;
-      `
-      : `cursor: pointer;`}
+  ${props =>
+  props.disabled
+    ? `
+    cursor: not-allowed;
+    `
+    : `cursor: pointer;`}
 `;
 
 type InputNumberAddonTextProps = {
 } & React.HTMLProps<HTMLElement> & React.HTMLAttributes<HTMLElement>;
 
 export const InputNumberAddonText = styled.span<InputNumberAddonTextProps>`
-    background-color: ${props => props.theme.forms.addonBackground} !important;
-    border: 1px solid ${props => props.theme.forms.border};
-    color: ${props => props.theme.forms.addonForeground} !important;
-    height: 50px;
-    line-height: 47px;
+  background-color: ${props => props.theme.forms.addonBackground} !important;
+  border: 1px solid ${props => props.theme.forms.border};
+  color: ${props => props.theme.forms.addonForeground} !important;
+  height: 50px;
+  line-height: 47px;
 
-    * {
-        color: ${props => props.theme.forms.addonForeground} !important;
-    }
-    ${props =>
-    props.disabled
-      ? `
-      cursor: not-allowed;
-      `
-      : `cursor: pointer;`}
+  * {
+      color: ${props => props.theme.forms.addonForeground} !important;
+  }
+  ${props =>
+  props.disabled
+    ? `
+    cursor: not-allowed;
+    `
+    : `cursor: pointer;`}
+`;
+
+export const StyledScanQRCode = styled(ScanQRCode)`
+  width: 50%;
+  font-size: 20px;
+  display: inline-flex;
+  border-right: 1px solid rgb(207, 199, 192);
+  height: 100%;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  align-items: center;
+  justify-content: center;
 `;
 
 type SendXpiInputProps = {
@@ -268,10 +282,16 @@ export const FormItemWithQRCodeAddon = (props: FormItemWithQRCodeAddonProps) => 
           prefix={<ThemedWalletOutlined />}
           autoComplete="off"
           addonAfter={
-            <ScanQRCode
-              loadWithCameraOpen={loadWithCameraOpen}
-              onScan={onScan}
-            />
+            <>
+              <StyledScanQRCode
+                loadWithCameraOpen={loadWithCameraOpen}
+                onScan={onScan}
+              />
+              <UploadQRCode
+                onScan={onScan}
+                codeType='address'
+              />
+            </>
           }
           {...inputProps}
         />
@@ -281,12 +301,14 @@ export const FormItemWithQRCodeAddon = (props: FormItemWithQRCodeAddonProps) => 
 };
 
 type FormItemClaimCodeXpiInputProps = {
+  onScan: Function;
+  loadWithCameraOpen: boolean;
   inputProps: InputProps;
 } & FormItemProps;
 
 export const FormItemClaimCodeXpiInput = (props: FormItemClaimCodeXpiInputProps) => {
 
-  const { inputProps, ...otherProps } = props;
+  const { onScan, loadWithCameraOpen, inputProps, ...otherProps } = props;
   return (
     <AntdFormWrapper {...otherProps}>
       <Form.Item {...otherProps}>
@@ -295,6 +317,18 @@ export const FormItemClaimCodeXpiInput = (props: FormItemClaimCodeXpiInputProps)
           placeholder="Claim Code"
           name="claimCode"
           autoComplete="off"
+          addonAfter={
+            <>
+              <StyledScanQRCode
+                loadWithCameraOpen={loadWithCameraOpen}
+                onScan={onScan}
+              />
+              <UploadQRCode
+                onScan={onScan}
+                codeType='claimCode'
+              />
+            </>
+          }
           required
           {...inputProps}
         />
---
+++ packages/lixi-components/src/components/Common/UploadQRCode.tsx
@@ -0,0 +1,137 @@
+import React, { useState } from 'react';
+import { Upload, message } from 'antd';
+import { UploadOutlined } from '@ant-design/icons';
+import styled from 'styled-components';
+import { BrowserQRCodeReader } from '@zxing/library';
+
+import {
+  isValidLotusPrefix
+} from './Ticker';
+
+const StyledUploadOutlined = styled(UploadOutlined)`
+  color: ${props => props.theme.primary} !important;
+  font-size: 20px;
+`
+
+const StyledSpan = styled.span`
+  width: 50%;
+  font-size: 15px;
+  display: inline-flex;
+  height: 100%;
+  position: absolute;
+  top: 0px;
+  align-items: center;
+  justify-content: center;
+  color: ${props => props.theme.primary} !important;
+`;
+
+type UploadQRCodeProps = {
+  codeType: String,
+  onScan: Function;
+}
+
+const UploadQRCode = (props: UploadQRCodeProps) => {
+  const { codeType, onScan, ...otherProps } = props;
+  const [activeCodeReader, setActiveCodeReader] = useState(null);
+
+  const [defaultFileList, setDefaultFileList] = useState([]);
+  const [error, setError] = useState(false);
+
+  const uploadProps = {
+    accept: "image/*",
+    showUploadList: false,
+    defaultFileList: defaultFileList,
+    className: "image-upload-grid",
+    maxCount: 1,
+
+    beforeUpload: file => {
+      const reader = new FileReader();
+
+      reader.onload = async (e) => {
+        const imageUrl = e.target.result;
+        scanQrFromImage(imageUrl);
+      };
+      reader.readAsDataURL(file);
+
+      // Prevent upload
+      return false;
+    }
+  };
+
+  const teardownCodeReader = codeReader => {
+    if (codeReader !== null) {
+      codeReader.reset();
+      codeReader.stop();
+      codeReader = null;
+      setActiveCodeReader(codeReader);
+    }
+  };
+
+  const parseAddressContent = content => {
+    let type = 'unknown';
+    let values = {};
+    let value = content;
+
+    // If what scanner reads from QR code begins with 'bitcoincash:' or 'simpleledger:' or their successor prefixes
+    if (isValidLotusPrefix(content)) {
+      type = 'address';
+      values = { address: content };
+    }
+    return { type, values };
+  };
+
+  // Have to apply logic parse redeem code
+  const parseRedeemContent = content => {
+    let type = 'claimCode';
+    let values = {
+      result: content
+    };
+    return { type, values };
+  };
+
+  const scanQrFromImage = async (imageUrl) => {
+    const codeReader = new BrowserQRCodeReader();
+    setActiveCodeReader(codeReader);
+
+    try {
+      const content = await codeReader.decodeFromImage(undefined, imageUrl);
+      let result = null;
+
+      switch (codeType) {
+        case 'address': result = parseAddressContent(content.getText());
+          break;
+        case 'claimCode': result = parseRedeemContent(content.getText());
+          break;
+
+        default:
+          break;
+      }
+
+      // stop scanning and fill form if it's an address
+      if (result?.type === codeType) { // Hide the scanner
+        onScan(result.values?.result);
+        return teardownCodeReader(codeReader);
+      }
+    } catch (err) {
+      console.log(`Error in QR scanner:`);
+      console.log(err);
+      console.log(JSON.stringify(err.message));
+      // setMobileErrorMsg(JSON.stringify(err.message));
+      message.error(`Couldn't scan QR code from uploaded image`);
+      setError(err);
+      teardownCodeReader(codeReader);
+    }
+  };
+
+  return (
+    <>
+      <StyledSpan>
+        <Upload {...uploadProps}>
+          <StyledUploadOutlined />
+        </Upload>
+      </StyledSpan>
+    </>
+  )
+};
+
+export default UploadQRCode;
