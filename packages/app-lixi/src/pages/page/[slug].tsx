b6f3638ddbba8999fd1b6bddc5b097c51eca11bb
Ericson <phamnguyentanh.97@gmail.com>
2022-07-19 15:12:09 +0700

    edit slug

--- packages/app-lixi/src/pages/page/[slug].tsx
+++ packages/app-lixi/src/pages/page/[slug].tsx
@@ -6,10 +6,6 @@ import React from 'react';
 import { END } from 'redux-saga';
 import { getSelectorsByUserAgent } from 'react-device-detect';
 import PageDetailLayout from '@components/Layout/PageDetailLayout';
-<<<<<<< HEAD
-import CreatePageComponent from '@components/Pages/CreatePageComponent';
-=======
->>>>>>> master
 
 const PageDetailPage = props => {
   const { shopId, isMobile } = props;
