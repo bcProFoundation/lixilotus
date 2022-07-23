d0bdf90e189d0908da6512c2c9ef2e7f6246d16e
KenNguyen-dev <74177525+KenNguyen-dev@users.noreply.github.com>
2022-07-23 11:19:53 +0700

    Fix receive image

--- packages/app-lixi-api/src/modules/core/core.module.ts
+++ packages/app-lixi-api/src/modules/core/core.module.ts
@@ -89,6 +89,7 @@ const baseCorsConfig = cors({
     ClaimController,
     EnvelopeController,
     HeathController,
+    UploadFilesController
   ],
   providers: [
     LixiService,
--- packages/app-lixi-api/src/modules/core/upload/upload.controller.ts
+++ packages/app-lixi-api/src/modules/core/upload/upload.controller.ts
@@ -1,26 +1,32 @@
-import { Controller, Get, Res, Param, HttpException, HttpStatus, Query } from '@nestjs/common';
+import { Controller, Get, Res, Param, HttpException, HttpStatus, Query, StreamableFile, Header } from '@nestjs/common';
 import { VError } from 'verror';
 import { I18n, I18nContext } from 'nestjs-i18n';
+import { createReadStream } from 'fs';
+import { join } from 'path';
 
 @Controller('uploads')
 export class UploadFilesController {
     @Get('/:id?')
+    @Header('Content-Type', 'image/png')
     async getImage(
         @Param('id') id: string,
-        @Res() res: any,
+        @Res({ passthrough: true }) res: StreamableFile,
         @Query('fileId') fileId: any,
         @I18n() i18n: I18nContext,
     ) {
         try {
-            res.sendFile(fileId, {root: `public/uploads/${id}`})
+            const path = join(process.cwd(), `public/uploads/${id}/${fileId}`);
+            const file = createReadStream(path);
+
+            return new StreamableFile(file);
         } catch (err) {
-        if (err instanceof VError) {
-            throw new HttpException(err, HttpStatus.INTERNAL_SERVER_ERROR);
-        } else {
-            const unableToUpload = await i18n.t('lixi.messages.unableToGet');
-            const error = new VError.WError(err as Error, unableToUpload);
-            throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+            if (err instanceof VError) {
+                throw new HttpException(err, HttpStatus.INTERNAL_SERVER_ERROR);
+            } else {
+                const unableToUpload = await i18n.t('lixi.messages.unableToGet');
+                const error = new VError.WError(err as Error, unableToUpload);
+                throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR);
+            }
         }
     }
 }
