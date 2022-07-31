c9d19eb1c4885e1874a78e1a2ad37a39a076fd38
tunghoang <thtung2901@gmail.com>
2022-07-31 10:52:48 +0700

    add logz.io transport

--- packages/app-lixi-api/src/logger.config.ts
+++ packages/app-lixi-api/src/logger.config.ts
@@ -1,16 +1,35 @@
 import { WinstonModule } from 'nest-winston';
 import winston, { format } from 'winston';
+import LogzioWinstonTransport from 'winston-logzio';
 import 'winston-daily-rotate-file';
 const { combine, timestamp, printf } = format;
 
-export const transport = new winston.transports.DailyRotateFile({
-  filename: 'lixi-api-%DATE%.log',
-  handleExceptions: true,
-  maxSize: '40m',
-  maxFiles: '14d',
-  dirname: './logs',
-  level: 'debug' // TODO
-});
+const environment = process.env.DEPLOY_ENVIRONMENT;
+const nodeEnv = process.env.NODE_ENV;
+
+const allTransports: winston.transport[] = [
+  new winston.transports.DailyRotateFile({
+    filename: 'lixi-api-%DATE%.log',
+    handleExceptions: true,
+    maxSize: '40m',
+    maxFiles: '14d',
+    dirname: './logs',
+    level: 'debug' // TODO
+  })
+];
+
+if (nodeEnv !== 'development') {
+  const logzioWinstonTransport = new LogzioWinstonTransport({
+    level: 'info',
+    name: 'winston_logzio',
+    token: 'cyUCOLibVZzYPMorIQOoMjFnPBgHzUQi',
+    host: 'listener-au.logz.io',
+    extraFields: {
+      environment: `${environment}`
+    }
+  });
+  allTransports.push(logzioWinstonTransport);
+}
 
 export const formatTimestamp = (date: Date): string =>
   `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date
@@ -28,17 +47,16 @@ export const formatTimestamp = (date: Date): string =>
 // export const timestamp = () => formatTimestamp(new Date());
 
 export const loggerConfig = WinstonModule.createLogger({
-  transports: [transport],
+  transports: allTransports,
   exceptionHandlers: [new winston.transports.File({ filename: 'exceptions.log', dirname: './logs' })],
   exitOnError: false,
   format: combine(
     format.errors({ stack: true }), // log the full stack
     timestamp(), // get the time stamp part of the full log message
     printf(({ level, message, context, stack, timestamp }) => {
-      const logLevel = level ?? '';
       const logStack = stack ? ` - stack: ${stack} ` : '';
       // formating the log outcome to show/store
-      const logMessage: string = `${timestamp} [${context}] ${level ?? ''}: ${message} ${logStack}`;
+      const logMessage: string = `[${environment}] ${timestamp} [${context}] ${level ?? ''}: ${message} ${logStack}`;
       if (level == 'error') {
         console.log(logMessage);
       }
