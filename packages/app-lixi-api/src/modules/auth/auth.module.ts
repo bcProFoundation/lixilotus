51a75af534e209eaee63229d12dbcdc7fb66f7ea
KenNguyen-dev <74177525+KenNguyen-dev@users.noreply.github.com>
2022-09-10 00:15:22 +0700

    Add jwt password

--- packages/app-lixi-api/src/modules/auth/auth.module.ts
+++ packages/app-lixi-api/src/modules/auth/auth.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [],
+  imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
   controllers: [AuthController],
-  providers: [AuthService],
-  exports: [AuthService]
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService, JwtStrategy]
 })
 export class AuthModule {}
