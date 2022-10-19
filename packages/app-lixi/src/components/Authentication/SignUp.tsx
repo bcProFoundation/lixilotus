97d81958229d7b6dfa3dfb2ce5124f19493d0f54
Trung <30361153+doantrung1997@users.noreply.github.com>
2022-10-19 20:14:28 +0700

    Fixed search lixi on Desktop mode (#231)
    * update lixi management
    
    * fix selected lixi
    
    * Resolve review
    
    * add yarn.lock
    
    * Update search lixi
    
    * Fixed can not search lixi Desktop mode and edit UI for mobile
    
    Co-authored-by: trung.doan1 <trung.doan@koidra.ai>

--- packages/app-lixi/src/components/Authentication/SignUp.tsx
+++ packages/app-lixi/src/components/Authentication/SignUp.tsx
@@ -16,51 +16,50 @@ const StyledSignUp = styled.div`
   }
 `;
 
-const
-  SignUp = () => {
-    const authentication = useContext(AuthenticationContext);
+const SignUp = () => {
+  const authentication = useContext(AuthenticationContext);
 
-    const handleSignUp = async () => {
-      try {
-        await authentication.signUp();
-      } catch (err) {
-        Modal.error({
-          title: 'Registration Error',
-          content: 'Cannot create Credential on your device',
-          centered: true
-        });
-      }
-    };
-
-    let signUpBody;
-    if (authentication) {
-      signUpBody = (
-        <div>
-          <p>Enable wallet lock to protect your funds.</p>
-          <p>
-            You will need to unlock with your <strong>fingerprint / device pin</strong> in order to access the wallet.
-          </p>
-          <p>
-            This lock can also be enabled / disabled under
-            <br />
-            <strong>Settings / General Settings / App Lock</strong>
-          </p>
-          <PrimaryButton onClick={handleSignUp}>Enable Lock</PrimaryButton>
-          <SecondaryButton onClick={() => authentication.turnOffAuthentication()}>Skip</SecondaryButton>
-        </div>
-      );
-    } else {
-      signUpBody = <p>Authentication is not supported</p>;
+  const handleSignUp = async () => {
+    try {
+      await authentication.signUp();
+    } catch (err) {
+      Modal.error({
+        title: 'Registration Error',
+        content: 'Cannot create Credential on your device',
+        centered: true
+      });
     }
-
-    return (
-      <StyledSignUp>
-        <h2>
-          <ThemedLockOutlined /> Wallet Lock
-        </h2>
-        {signUpBody}
-      </StyledSignUp>
-    );
   };
+
+  let signUpBody;
+  if (authentication) {
+    signUpBody = (
+      <div>
+        <p>Enable wallet lock to protect your funds.</p>
+        <p>
+          You will need to unlock with your <strong>fingerprint / device pin</strong> in order to access the wallet.
+        </p>
+        <p>
+          This lock can also be enabled / disabled under
+          <br />
+          <strong>Settings / General Settings / App Lock</strong>
+        </p>
+        <PrimaryButton onClick={handleSignUp}>Enable Lock</PrimaryButton>
+        <SecondaryButton onClick={() => authentication.turnOffAuthentication()}>Skip</SecondaryButton>
+      </div>
+    );
+  } else {
+    signUpBody = <p>Authentication is not supported</p>;
+  }
+
+  return (
+    <StyledSignUp>
+      <h2>
+        <ThemedLockOutlined /> Wallet Lock
+      </h2>
+      {signUpBody}
+    </StyledSignUp>
+  );
+};
 
 export default SignUp;
