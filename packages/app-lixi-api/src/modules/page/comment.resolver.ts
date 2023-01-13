a371e051b76efbdf5d975e0b934348599411cc89
khanhvinhnguyen <khanhvinh.nguyen.0628@gmail.com>
2023-01-13 14:57:16 +0700

    issue-383: Give tips in comments

--- packages/app-lixi-api/src/modules/page/comment.resolver.ts
+++ packages/app-lixi-api/src/modules/page/comment.resolver.ts
@@ -6,12 +6,17 @@ import {
   CreateCommentInput,
   PaginationArgs
 } from '@bcpros/lixi-models';
+import BCHJS from '@bcpros/xpi-js';
 import { findManyCursorConnection } from '@devoxa/prisma-relay-cursor-connection';
-import { Logger, UseGuards } from '@nestjs/common';
+import { HttpException, HttpStatus, Inject, Logger, UseGuards } from '@nestjs/common';
 import { Args, Mutation, Parent, Query, ResolveField, Resolver, Subscription } from '@nestjs/graphql';
+import { ChronikClient } from 'chronik-client';
 import { PubSub } from 'graphql-subscriptions';
+import _ from 'lodash';
 import { I18n, I18nService } from 'nestjs-i18n';
+import { InjectChronikClient } from 'src/common/modules/chronik/chronik.decorators';
 import { PostAccountEntity } from 'src/decorators/postAccount.decorator';
+import VError from 'verror';
 import { GqlJwtAuthGuard } from '../auth/guards/gql-jwtauth.guard';
 import { PrismaService } from '../prisma/prisma.service';
 
@@ -21,7 +26,12 @@ const pubSub = new PubSub();
 export class CommentResolver {
   private logger: Logger = new Logger(this.constructor.name);
 
-  constructor(private prisma: PrismaService, @I18n() private i18n: I18nService) {}
+  constructor(
+    private prisma: PrismaService,
+    @I18n() private i18n: I18nService,
+    @InjectChronikClient('xpi') private chronik: ChronikClient,
+    @Inject('xpijs') private XPI: BCHJS
+  ) {}
 
   @Subscription(() => Comment)
   commentCreated() {
@@ -89,26 +99,67 @@ export class CommentResolver {
   @UseGuards(GqlJwtAuthGuard)
   @Mutation(() => Comment)
   async createComment(@PostAccountEntity() account: Account, @Args('data') data: CreateCommentInput) {
-    if (!account) {
-      const couldNotFindAccount = await this.i18n.t('post.messages.couldNotFindAccount');
-      throw new Error(couldNotFindAccount);
-    }
-
-    const { commentText, commentToId } = data;
-
-    const commentToSave = {
-      data: {
-        commentText: commentText,
-        commentAccount: { connect: { id: account.id } },
-        commentTo: {
-          connect: { id: commentToId }
-        }
+    try {
+      if (!account) {
+        const couldNotFindAccount = await this.i18n.t('post.messages.couldNotFindAccount');
+        throw new Error(couldNotFindAccount);
       }
-    };
-    const createdComment = await this.prisma.comment.create(commentToSave);
 
-    pubSub.publish('commentCreated', { commentCreated: createdComment });
-    return createdComment;
+      const { commentText, commentToId, tipHex } = data;
+
+      const commentToSave = {
+        data: {
+          commentText: commentText,
+          commentAccount: { connect: { id: account.id } },
+          commentTo: {
+            connect: { id: commentToId }
+          }
+        }
+      };
+
+      const savedComment = await this.prisma.$transaction(async prisma => {
+        const createdComment = await prisma.comment.create(commentToSave);
+
+        if (tipHex) {
+          const post = await prisma.post.findFirst({
+            where: {
+              id: commentToId
+            },
+            include: {
+              postAccount: true
+            }
+          });
+
+          const txData = await this.XPI.RawTransactions.decodeRawTransaction(tipHex);
+          const { value } = txData['vout'][0];
+          if (Number(value) < 0) {
+            throw new Error('Syntax error. Number cannot be less than or equal to 0');
+          }
+
+          const broadcastResponse = await this.chronik.broadcastTx(tipHex);
+          const { txid } = broadcastResponse;
+          const transactionTip = {
+            txid,
+            fromAddress: account.address,
+            fromAccountId: account.name,
+            toAddress: post?.postAccount.address as string,
+            toAccountId: post?.postAccount.name as string,
+            tipValue: value,
+            commentId: createdComment.id
+          };
+
+          await prisma.giveTip.create({ data: transactionTip });
+        }
+        return createdComment;
+      });
+
+      pubSub.publish('commentCreated', { commentCreated: savedComment });
+      return savedComment;
+    } catch (err) {
+      if (err instanceof VError) {
+        throw new HttpException(err, HttpStatus.INTERNAL_SERVER_ERROR);
+      }
+    }
   }
 
   @ResolveField('commentAccount', () => Account)
