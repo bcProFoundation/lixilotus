2d8f7326b0358cdca663c4a3c45467b294ca5860
Nhan <95977133+tlnhan@users.noreply.github.com>
2023-03-23 11:26:53 +0700

    Fix img dev lixilotus (#542)
    * issue 497, responsive img for screens other

--- packages/app-lixi/src/components/Posts/PostListItem.tsx
+++ packages/app-lixi/src/components/Posts/PostListItem.tsx
@@ -248,10 +248,13 @@ const PostListItem = ({ index, item, searchValue, handleBurnForPost }: PostListI
 
   useEffect(() => {
     const mapImages = item.uploads.map(img => {
+      //Resize img with Sha
       let imgSha;
-      if (width <= 1200) {
+      if (!img.upload.sha800 || !img.upload.sha320 || !img.upload.sha40) {
+        imgSha = img.upload.sha;
+      } else if (width <= 1200) {
         imgSha = img.upload.sha320;
-      } else {
+      } else if (width > 1200) {
         imgSha = img.upload.sha800;
       }
       const imgUrl = `${process.env.NEXT_PUBLIC_AWS_ENDPOINT}/${img.upload.bucket}/${imgSha}`;
