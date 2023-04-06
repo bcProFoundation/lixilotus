ccf046b97fb997326887ccff52c5822f61cda469
Bo2208 <101168030+Bo2208@users.noreply.github.com>
2023-04-06 21:32:58 +0700

    issue549 (#578)

--- packages/app-lixi/src/components/Wallet/FullWallet.tsx
+++ packages/app-lixi/src/components/Wallet/FullWallet.tsx
@@ -171,7 +171,12 @@ const FullWalletComponent: React.FC = () => {
     let idComment = burnForId;
 
     if (burnForType == BurnForType.Token && burnForId.length !== 64) {
-      burnForId = allTokens.find(token => token.id === burnForId).tokenId;
+      const searchTokenID = allTokens.find(token => token.id === burnForId);
+      if (searchTokenID) {
+        burnForId = searchTokenID.tokenId;
+      } else {
+        return '/404';
+      }
     }
     if (burnForType == BurnForType.Comment) {
       burnForTypeString = getBurnForType(BurnForType.Post);
