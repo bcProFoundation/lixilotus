df492cb8529b3401faee459e044bdaad7bf79c8d
KenNguyen-dev <74177525+KenNguyen-dev@users.noreply.github.com>
2023-04-15 09:01:28 +0700

    Add gallery to app-lotus-temple

--- packages/app-lotus-temple/src/pages/_app.tsx
+++ packages/app-lotus-temple/src/pages/_app.tsx
@@ -43,7 +43,7 @@ const LixiApp = ({ Component, ...rest }) => {
             <OutsideCallConsumer config={callConfig}>
               <Layout className="lixi-app-layout">
                 <Head>
-                  <title>LixiLotus</title>
+                  <title>Lotus Temple</title>
                   <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
                   <link
                     rel="stylesheet"
