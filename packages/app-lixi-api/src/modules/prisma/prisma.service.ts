66edddbd6b2a1dac106cf17924e18e713c0f109f
vince8x <vince.tran8x@gmail.com>
2023-06-02 14:56:15 +0700

    Fix caching page

--- packages/app-lixi-api/src/modules/prisma/prisma.service.ts
+++ packages/app-lixi-api/src/modules/prisma/prisma.service.ts
@@ -18,11 +18,6 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
           model: 'NotificationType',
           action: 'findFirst',
           ttl: 300
-        },
-        {
-          model: 'Page',
-          action: 'findFirst',
-          ttl: 30
         }
       ]
     });
--- packages/lixi-models/src/core/scalars/date.scalar.ts
+++ packages/lixi-models/src/core/scalars/date.scalar.ts
@@ -3,15 +3,15 @@ import { Kind } from 'graphql';
 import moment from 'moment';
 
 @Scalar('Date', () => Date)
-export class DateScalar implements CustomScalar<string, Date> {
+export class DateScalar implements CustomScalar<Date, Date> {
   description = 'Date custom scalar type';
 
   parseValue(value: string): Date {
     return moment(value).toDate(); // value from the client
   }
 
-  serialize(value: Date): string {
-    return moment(value).toISOString(); // value sent to the client
+  serialize(value: any): Date {
+    return moment(value).toDate(); // value sent to the client
   }
 
   parseLiteral(ast: any): Date {
