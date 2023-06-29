e815a886e478ea6036a6b71f43354d32ae869cf9
vince8x <vince.tran8x@gmail.com>
2023-06-29 10:52:14 +0700

    fix and logging cors

--- packages/app-lixi-api/src/main.ts
+++ packages/app-lixi-api/src/main.ts
@@ -58,7 +58,7 @@ async function bootstrap() {
         if (!origin) return callback(null, true);
         if (allowedOrigins.indexOf(origin) === -1) {
           const msg = `The CORS policy for this site does not allow access from the specified Origin. ${origin}`;
-          Logger.warn(msg)
+          console.log(msg);
           return callback(new Error(msg), false);
         }
         return callback(null, true);
