34ae451b8f8739b54ba439b794eb613b6e6a5459
vince8x <vince.tran8x@gmail.com>
2023-07-05 13:58:07 +0700

    Fix upload images

--- packages/app-lixi-api/src/common/modules/cloudflare/cloudflare.module.ts
+++ packages/app-lixi-api/src/common/modules/cloudflare/cloudflare.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CloudflareImagesService } from './cloudflare-images.service';
-import { CloudflareMigrateService } from './cloudflare-migrate.service';
 
 @Module({
   imports: [],
   controllers: [],
-  providers: [CloudflareImagesService, CloudflareMigrateService],
+  providers: [CloudflareImagesService],
   exports: [CloudflareImagesService]
 })
 export class CloudflareModule {}
